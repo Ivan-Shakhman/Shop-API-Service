4d0c0b314210df91d635bc4fe5b956d2fd5236fd
Ivan Shakhman <shakhmanivan@gmail.com>
2025-01-09 20:02:46 +0200

    add model for category

--- db/models.py
+++ db/models.py
@@ -1,1 +1,10 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Category(DeclarativeBase):
+    __tablename__ = "category"
+
+    id: Mapped[int] = mapped_column(index=True, primary_key=True)
+    name: Mapped[str]
+    age_limit: Mapped[int]
+
