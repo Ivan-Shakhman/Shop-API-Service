a01ca21dcaf85697aa3f76d9d4d6b8038b7f3b15
Ivan Shakhman <shakhmanivan@gmail.com>
2025-01-09 20:20:52 +0200

    add product model

--- db/models.py
+++ db/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from decimal import Decimal
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import Integer, ForeignKey
 
 
 class Category(DeclarativeBase):
@@ -16,3 +18,15 @@ class Distributor(DeclarativeBase):
     name: Mapped[str]
     country: Mapped[str]
 
+
+class Product(DeclarativeBase):
+    __tablename__ = "product"
+
+    id: Mapped[int] = mapped_column(index=True, primary_key=True)
+    name: Mapped[str]
+    price: Mapped[Decimal]
+    rel_category_id: Mapped[int] = mapped_column(Integer, ForeignKey("category.id"))
+    rel_distributor_id: Mapped[int] = mapped_column(Integer, ForeignKey("distributor.id"))
+
+    rel_category = relationship("category")
+    rel_distributor = relationship("distributor")
